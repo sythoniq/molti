93af4039c88dbf9f5238207ae892aba5354efaa4
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-07-30 22:03:09 +0300

    to pick up from Identifiers and keywords

--- src/scanner.c
+++ src/scanner.c
@@ -84,7 +84,29 @@ static void skipWhiteSpace() {
 	}
 }
 
+static TokenType checkKeyword(int start, int length, 
+															const char *rest, TokenType type) {
+	if (scanner.current - scanner.start == start + length && memcmp(scanner.start + start, rest, length) == 0) {
+		return type;
+	}
+	return TOKEN_IDENTIFIER;
+}
+
 static TokenType identifierType() {
+	switch (scanner.start[0]) {
+		case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+		case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+		case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+		case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+		case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+		case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+		case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+		case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+		case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+		case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+		case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+	}
+
 	return TOKEN_IDENTIFIER;
 }
 
@@ -121,7 +143,7 @@ static Token string() {
 
 static Token identifier() {
 	while (isAlpha(peek()) || isDigit(peek())) advance();
-	return makeToken(identifierType())
+	return makeToken(identifierType());
 }
 
 void initScanner(const char* source) {
