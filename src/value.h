601fac7fd9edc06d0a7f82f45135464ddd201923
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-08-04 19:18:57 +0300

    working thru adding value handling need to ponder more on this and how C works with the memory of unions n such

--- src/value.h
+++ src/value.h
@@ -9,6 +9,31 @@ typedef struct {
 				Value *values;
 } ValueArray;
 
+typedef enum {
+	VAL_BOOL,
+	VAL_NIL,
+	VAL_NUMBER
+} ValueType;
+
+typedef struct {
+	ValueType type;
+	union {
+		bool boolean;
+		double number;
+	} as;
+} Value;
+
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
 void initValueArray(ValueArray *arr);
 void writeValueArray(ValueArray *arr, Value value);
 void printValue(Value value);
